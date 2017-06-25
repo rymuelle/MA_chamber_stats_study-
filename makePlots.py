da3a9d7c42a82503d4ff410e2c274f995476c8bd
rymuelle <rmueller@physics.tamu.edu>
2017-06-26 00:50:36 +0200

    added histogram wheel and sector class

--- makePlots.py
+++ makePlots.py
@@ -20,10 +20,12 @@ class Chamber:
 		self.phiz = child['phiz']
 
 class ChamberInfo:
-	def __init__(self, name):
+	def __init__(self, name, reports, xml):
 		self.name = name
 		self.chambers = []
-		
+		tree = ET.ElementTree(file=xml)
+		root = tree.getroot()
+		self.set_values(reports, root)
 
 	def set_values(self,reports, root):
 		for count,report in enumerate(reports):
@@ -36,38 +38,77 @@ class ChamberInfo:
 						self.chambers.append(Chamber(report,child.attrib))
 					count = count + 1
 				
-		#for count, chamber in enumerate(self.chambers):
-		#	print chamber.detector, chamber.wheel, chamber.station, chamber.detector, chamber.stats, chamber.x
-      
+		for count, chamber in enumerate(self.chambers):
+			print chamber.detector, chamber.wheel, chamber.station, chamber.sector, chamber.stats, chamber.x
 
 
+class WheelSectorHistograms:
+	def __init__(self, name, chamber_class):
+		self.name = name
+		self.TH1F_sector_x = []
+		for wheel in range(5):
+			self.TH1F_sector_x.append([])
+			for station in range(4):
+				self.TH1F_sector_x[wheel].append( r.TH2F("TH1F_sector_x_{}_{}".format(wheel-2,station+1),"wheel {} station {}".format(wheel-2,station+1), 100, 0, 20000, 100, -.3,.3 ) )
 
-example = ChamberInfo("test")
+		for count, chamber in enumerate(chamber_class.chambers):
+			#print chamber.wheel, chamber.station, chamber.sector, chamber.x
+			self.TH1F_sector_x[chamber.wheel+2][chamber.station-1].Fill(float(chamber.stats), float(chamber.x))
+
+	def draw_hists(self):
+		for wheel in range(5):
+			for station in range(4):
+				self.TH1F_sector_x[wheel][station].Draw()
+				c1.SaveAs("output/TH1F_sector_x_{}_{}.png".format(wheel-2,station+1))
+				print self.TH1F_sector_x[wheel][station].GetRMS()
+	
+
 
 
 execfile("mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01_report.py")
-tree = ET.parse('mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01.xml')
-tree = ET.ElementTree(file='mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01.xml')
-root = tree.getroot()
+example = ChamberInfo("test", reports, "mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01.xml")
 
-example.set_values(reports, root)
+
+#execfile("mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01_report.py")
+#tree = ET.parse('mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01.xml')
+#tree = ET.ElementTree(file='mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01.xml')
+#root = tree.getroot()
+
+#example.set_values(reports, root)
 
 c1 = r.TCanvas()
-TH1F_sector_x = []
-
-for wheel in range(5):
-	TH1F_sector_x.append([])
-	for station in range(4):
-		TH1F_sector_x[wheel].append( r.TH1F("TH1F_sector_x_{}_{}".format(wheel-2,station+1),"wheel {} station {}".format(wheel-2,station+1), 100, -1,1 ) )
-
-for count, chamber in enumerate(example.chambers):
-	#print chamber.wheel, chamber.station, chamber.sector, chamber.x
-	TH1F_sector_x[chamber.wheel+2][chamber.station-1].Fill(float(chamber.x))
 
 
-for wheel in range(5):
-	for station in range(4):
-		TH1F_sector_x[wheel][station].Draw()
-		c1.SaveAs("output/TH1F_sector_x_{}_{}.png".format(wheel-2,station+1))
+example2 = WheelSectorHistograms("example", example)
 
+example2.draw_hists()
 
+#for wheel in range(5):
+#	TH1F_sector_x.append([])
+#	for station in range(4):
+#		TH1F_sector_x[wheel].append( r.TH2F("TH1F_sector_x_{}_{}".format(wheel-2,station+1),"wheel {} station {}".format(wheel-2,station+1), 100, 0, 20000, 100, -.3,.3 ) )
+#
+#for count, chamber in enumerate(example.chambers):
+#	#print chamber.wheel, chamber.station, chamber.sector, chamber.x
+#	TH1F_sector_x[chamber.wheel+2][chamber.station-1].Fill(float(chamber.stats), float(chamber.x))
+#
+#
+#for wheel in range(5):
+#	for station in range(4):
+#		TH1F_sector_x[wheel][station].Draw()
+#		c1.SaveAs("output/TH1F_sector_x_{}_{}.png".format(wheel-2,station+1))
+#		print TH1F_sector_x[wheel][station].GetRMS()
+#
+#
+#
+##TH2F_stats_v_x = r.TH2F("TH2F_stats_v_x", "TH2F_stats_v_x", 100, 0,20000, 100, -.3,.3)
+##
+##for count, chamber in enumerate(example.chambers):
+##	TH2F_stats_v_x.Fill(chamber.stats, abs(float(chamber.x)))
+##
+##print TH2F_stats_v_x.GetRMS(1), TH2F_stats_v_x.GetRMS(2)
+##
+##TH2F_stats_v_x.Draw("colz")
+#c1.SaveAs("output/TH2F_stats_v_x.png")
+#
+#
