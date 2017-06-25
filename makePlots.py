5708cb107324a83d725f2317de3e6d3021f3ab4c
rymuelle <rmueller@physics.tamu.edu>
2017-06-26 00:07:59 +0200

    now with basic 1d root plots

--- makePlots.py
+++ makePlots.py
@@ -1,6 +1,10 @@
 import ROOT as r 
 import xml.etree.ElementTree as ET
 
+
+r.gROOT.SetBatch(True)
+
+
 class Chamber:
 	def __init__(self, report, child):
 		self.detector = report.postal_address[0]
@@ -24,16 +28,16 @@ class ChamberInfo:
 	def set_values(self,reports, root):
 		for count,report in enumerate(reports):
 			if report.postal_address[0] == "DT":
-				print report.postal_address[1],report.postal_address[2], report.postal_address[3]
+				#print report.postal_address[1],report.postal_address[2], report.postal_address[3]
 				count = 0 
 				for child in root.findall("./operation/*[@wheel='{}'][@station='{}'][@sector='{}']/../setposition".format(report.postal_address[1],report.postal_address[2], report.postal_address[3])):
-					print child.tag, child.attrib
+					#print child.tag, child.attrib
 					if(count == 0): 
 						self.chambers.append(Chamber(report,child.attrib))
 					count = count + 1
 				
-		for count, chamber in enumerate(self.chambers):
-			print chamber.detector, chamber.wheel, chamber.station, chamber.detector, chamber.stats, chamber.x
+		#for count, chamber in enumerate(self.chambers):
+		#	print chamber.detector, chamber.wheel, chamber.station, chamber.detector, chamber.stats, chamber.x
       
 
 
@@ -47,4 +51,23 @@ tree = ET.ElementTree(file='mc_DT-1100-111111_CMSSW_9_2_1_13TeV_39M_01.xml')
 root = tree.getroot()
 
 example.set_values(reports, root)
+
+c1 = r.TCanvas()
+TH1F_sector_x = []
+
+for wheel in range(5):
+	TH1F_sector_x.append([])
+	for station in range(4):
+		TH1F_sector_x[wheel].append( r.TH1F("TH1F_sector_x_{}_{}".format(wheel-2,station+1),"wheel {} station {}".format(wheel-2,station+1), 100, -1,1 ) )
+
+for count, chamber in enumerate(example.chambers):
+	#print chamber.wheel, chamber.station, chamber.sector, chamber.x
+	TH1F_sector_x[chamber.wheel+2][chamber.station-1].Fill(float(chamber.x))
+
+
+for wheel in range(5):
+	for station in range(4):
+		TH1F_sector_x[wheel][station].Draw()
+		c1.SaveAs("output/TH1F_sector_x_{}_{}.png".format(wheel-2,station+1))
+
 
